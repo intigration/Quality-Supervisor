7cb6591d50a1525df6bef84e431c6b5a422249d0
Muhammad Farhan <intigration@gmail.com>
2024-06-13 13:26:59 +0500

    Update PSC.ts

--- src/types/PSC.ts
+++ src/types/PSC.ts
@@ -18,7 +18,7 @@ export interface Section {
   furtherResources?: Link[],
 }
 
-export type Priority = 'essential' | 'optional' | 'advanced'| 'dependancy';
+export type Priority = 'essential' | 'optional' | 'advanced';
 
 export interface Checklist {
   point: string,
