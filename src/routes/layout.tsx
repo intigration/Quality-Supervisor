fde0afcac017296a6704b1aaa407b57e554515dd
Muhammad Farhan <intigration@gmail.com>
2024-06-13 12:49:42 +0500

    Update layout.tsx

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -8,7 +8,7 @@ import { ChecklistContext } from "~/store/checklist-context";
 import type { Sections } from "~/types/PSC";
 
 export const useChecklists = routeLoader$(async () => {
-  const remoteUrl = 'http://localhost:5173/personal-security-checklist.yml';
+  const remoteUrl = 'https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/personal-security-checklist.yml';
   return fetch(remoteUrl)
     .then((res) => res.text())
     .then((res) => jsyaml.load(res) as Sections)
