77498531990d601a05bcbae2671b03631e7a2be5
Muhammad Farhan <intigration@gmail.com>
2024-06-16 05:46:47 +0500

    va

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -8,7 +8,7 @@ import { ChecklistContext } from "~/store/checklist-context";
 import type { Sections } from "~/types/PSC";
 
 export const useChecklists = routeLoader$(async () => {
-  const remoteUrl = 'http://localhost:5173/public/security.yml';
+  const remoteUrl = '/security.yml';
   return fetch(remoteUrl)
     .then((res) => res.text())
     .then((res) => jsyaml.load(res) as Sections)
