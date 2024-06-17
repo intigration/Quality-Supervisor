bc9e9305d0be1215dd7650b0ce2d0d31a7754add
Muhammad Farhan <intigration@gmail.com>
2024-06-17 19:24:42 +0500

    ba

--- src/data/tooling.ts
+++ src/data/tooling.ts
@@ -4,12 +4,130 @@ interface Tools {
     slug: string;
     markdown: string;
     warningMessage?: string;
-  }
-  
-  const tool: Tools[] = [{ "title": "1password.md", 
-    "description": "1password.md", 
-    "slug": "1password.md", 
-    "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/1password.md" }, { "title": "ansible.md", "description": "ansible.md", "slug": "ansible.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/ansible.md" }, { "title": "bitwarden.md", "description": "bitwarden.md", "slug": "bitwarden.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/bitwarden.md" }, { "title": "chocolatey.md", "description": "chocolatey.md", "slug": "chocolatey.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/chocolatey.md" }, { "title": "datree.md", "description": "datree.md", "slug": "datree.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/datree.md" }, { "title": "direnv.md", "description": "direnv.md", "slug": "direnv.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/direnv.md" }, { "title": "git.md", "description": "git.md", "slug": "git.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/git.md" }, { "title": "homebrew.md", "description": "homebrew.md", "slug": "homebrew.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/homebrew.md" }, { "title": "iterm2.md", "description": "iterm2.md", "slug": "iterm2.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/iterm2.md" }, { "title": "nmap.md", "description": "nmap.md", "slug": "nmap.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/nmap.md" }, { "title": "openssh.md", "description": "openssh.md", "slug": "openssh.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/openssh.md" }, { "title": "openssl.md", "description": "openssl.md", "slug": "openssl.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/openssl.md" }, { "title": "packer.md", "description": "packer.md", "slug": "packer.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/packer.md" }, { "title": "starship.md", "description": "starship.md", "slug": "starship.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/starship.md" }, { "title": "tcpdump.md", "description": "tcpdump.md", "slug": "tcpdump.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/tcpdump.md" }, { "title": "terraform.md", "description": "terraform.md", "slug": "terraform.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/terraform.md" }, { "title": "vagrant.md", "description": "vagrant.md", "slug": "vagrant.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/vagrant.md" }, { "title": "vscode.md", "description": "vscode.md", "slug": "vscode.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/vscode.md" }, { "title": "wireshark.md", "description": "wireshark.md", "slug": "wireshark.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/wireshark.md" }, { "title": "wsl.md", "description": "wsl.md", "slug": "wsl.md", "markdown": "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/wsl.md" }]
+}
+
+const tool: Tools[] = [
+    {
+      title: "1password.md",
+      description: "1password.md",
+      slug: "1password.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/1password.md"
+    },
+    {
+      title: "ansible.md",
+      description: "ansible.md",
+      slug: "ansible.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/ansible.md"
+    },
+    {
+      title: "bitwarden.md",
+      description: "bitwarden.md",
+      slug: "bitwarden.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/bitwarden.md"
+    },
+    {
+      title: "chocolatey.md",
+      description: "chocolatey.md",
+      slug: "chocolatey.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/chocolatey.md"
+    },
+    {
+      title: "datree.md",
+      description: "datree.md",
+      slug: "datree.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/datree.md"
+    },
+    {
+      title: "direnv.md",
+      description: "direnv.md",
+      slug: "direnv.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/direnv.md"
+    },
+    {
+      title: "git.md",
+      description: "git.md",
+      slug: "git.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/git.md"
+    },
+    {
+      title: "homebrew.md",
+      description: "homebrew.md",
+      slug: "homebrew.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/homebrew.md"
+    },
+    {
+      title: "iterm2.md",
+      description: "iterm2.md",
+      slug: "iterm2.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/iterm2.md"
+    },
+    {
+      title: "nmap.md",
+      description: "nmap.md",
+      slug: "nmap.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/nmap.md"
+    },
+    {
+      title: "openssh.md",
+      description: "openssh.md",
+      slug: "openssh.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/openssh.md"
+    },
+    {
+      title: "openssl.md",
+      description: "openssl.md",
+      slug: "openssl.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/openssl.md"
+    },
+    {
+      title: "packer.md",
+      description: "packer.md",
+      slug: "packer.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/packer.md"
+    },
+    {
+      title: "starship.md",
+      description: "starship.md",
+      slug: "starship.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/starship.md"
+    },
+    {
+      title: "tcpdump.md",
+      description: "tcpdump.md",
+      slug: "tcpdump.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/tcpdump.md"
+    },
+    {
+      title: "terraform.md",
+      description: "terraform.md",
+      slug: "terraform.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/terraform.md"
+    },
+    {
+      title: "vagrant.md",
+      description: "vagrant.md",
+      slug: "vagrant.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/vagrant.md"
+    },
+    {
+      title: "vscode.md",
+      description: "vscode.md",
+      slug: "vscode.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/vscode.md"
+    },
+    {
+      title: "wireshark.md",
+      description: "wireshark.md",
+      slug: "wireshark.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/wireshark.md"
+    },
+    {
+      title: "wsl.md",
+      description: "wsl.md",
+      slug: "wsl.md",
+      markdown: "https://raw.githubusercontent.com/intigration/Quality-Supervisor/main/public/tools/wsl.md"
+    }
+  ]
 
 
-    export default tool;
+export default tool;
