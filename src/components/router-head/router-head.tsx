4a449be016f29c4fb4015d30796425c3bd03fd76
Muhammad Farhan <intigration@gmail.com>
2024-06-20 12:15:09 +0500

    Update router-head.tsx

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -70,9 +70,9 @@ export const RouterHead = component$(() => {
         <style key={s.key} {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
 
-      {head.scripts.map((s) => (
+{/*       {head.scripts.map((s) => (
         <script key={s.key} {...s.props} dangerouslySetInnerHTML={s.script} />
-      ))}
+      ))} */}
       {/* <script defer data-domain="digital-defense.io" src="https://no-track.as93.net/js/script.js" /> */}
     </>
   );
